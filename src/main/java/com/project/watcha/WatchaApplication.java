aacb8453834f30898c772f09eda3aa9bcdd14f10
SungGil-5125 <sunggil0125@naver.com>
2022-07-27 12:55:38 +0900

    🎉 :: project setting

---
+++ src/main/java/com/project/watcha/WatchaApplication.java
@@ -0,0 +1,13 @@
+package com.project.watcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/watcha/WatchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.watcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
