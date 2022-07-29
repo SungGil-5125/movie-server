3b6c18eb3bae7a19bf50bf4ddfe35f8214b0b1b6
SungGil-5125 <sunggil0125@naver.com>
2022-07-29 22:14:34 +0900

    ✨ :: signin testCode

--- src/test/java/com/project/watcha/domain/sign/service/SignServiceImplTest.java
+++ src/test/java/com/project/watcha/domain/sign/service/SignServiceImplTest.java
@@ -1,14 +1,15 @@
-package com.project.watcha.domain.sign.service.Impl;
+package com.project.watcha.domain.sign.service;
 
+import com.project.watcha.domain.sign.dto.request.SignInDto;
 import com.project.watcha.domain.sign.dto.request.SignUpDto;
+import com.project.watcha.domain.sign.dto.response.SignInResponseDto;
 import com.project.watcha.domain.user.repository.UserRepository;
-import com.project.watcha.domain.sign.service.SignService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SignServiceImplTest {
@@ -31,5 +32,20 @@ class SignServiceImplTest {
 
         // then
         assertThat(register).isEqualTo(userRepository.findByEmail(signUpDto.getEmail()).orElseThrow().getUser_id());
+    }
+
+    @Test
+    @DisplayName("로그인 테스트")
+    void login() {
+
+        // given
+        SignInDto signInDto = new SignInDto("김성길", "s21023@gsm.hs.kr", "kimsunggil2005!");
+
+        // when
+        SignInResponseDto login = signService.login(signInDto);
+        System.out.println("login = " + login.getRefreshToken());
+
+        // then
+        assertThat(login).isNotNull();
     }
 }
