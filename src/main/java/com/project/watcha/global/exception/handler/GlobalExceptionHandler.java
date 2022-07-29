c0502d6a5cca991cb12d36b4d8652521068e7a21
SungGil-5125 <sunggil0125@naver.com>
2022-07-29 22:13:55 +0900

    ✨ :: Add MethodArgumentNotValidException

--- src/main/java/com/project/watcha/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/watcha/global/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,9 @@ import com.project.watcha.global.exception.exceptions.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -38,7 +41,23 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> processValidationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        StringBuilder stringBuilder = new StringBuilder();
 
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            stringBuilder.append(fieldError.getField()).append(" : ");
+            stringBuilder.append(fieldError.getDefaultMessage());
+            stringBuilder.append(", ");
+        }
+
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(", "));
+        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(" "));
+        ErrorResponse errorResponse = new ErrorResponse(stringBuilder.toString(), HttpStatus.BAD_REQUEST.value());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 
     public void printException(HttpServletRequest request, ErrorCode errorCode) {
         log.error("[ " + request.getRequestURI() + " 에서 { " + errorCode + " } 발생 ]");
