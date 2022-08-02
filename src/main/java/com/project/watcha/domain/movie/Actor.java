22d3bca0a7fbc9721abb0ba000f771cb379ee9f8
SungGil-5125 <sunggil0125@naver.com>
2022-08-02 22:18:44 +0900

    ✨ :: Add image url

--- src/main/java/com/project/watcha/domain/movie/Actor.java
+++ src/main/java/com/project/watcha/domain/movie/Actor.java
@@ -1,16 +1,10 @@
 package com.project.watcha.domain.movie;
 
-import com.project.watcha.domain.movie.enumType.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static javax.persistence.EnumType.STRING;
 
 @Entity
 @AllArgsConstructor
@@ -25,8 +19,6 @@ public class Actor {
     @Column
     private String name;
 
-    @Enumerated(STRING) @Column(name = "genre")
-    @ElementCollection(fetch = FetchType.EAGER) // 바로 가져오기
-    @CollectionTable(name = "actor_gender", joinColumns = @JoinColumn(name = "actor_id"))
-    private List<Gender> gender = new ArrayList<>();
+    @Column
+    private String url;
 }
--- src/main/java/com/project/watcha/domain/movie/Director.java
+++ src/main/java/com/project/watcha/domain/movie/Director.java
@@ -1,15 +1,10 @@
 package com.project.watcha.domain.movie;
 
-import com.project.watcha.domain.movie.enumType.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import static javax.persistence.EnumType.STRING;
 
 @Entity
 @AllArgsConstructor
@@ -24,10 +19,9 @@ public class Director {
     @Column
     private String name;
 
-    @Enumerated(STRING) @Column(name = "genre")
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "director_gender", joinColumns = @JoinColumn(name = "director_id"))
-    private List<Gender> gender = new ArrayList<>();
+    @Column
+    private String url;
 
 
+           
 }
