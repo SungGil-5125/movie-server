d987a6f523b934e8ad4e3a58926fdf3b17a0e06c
SungGil-5125 <sunggil0125@naver.com>
2022-08-03 21:08:06 +0900

    ✨ :: Add uploadMovie

--- src/main/java/com/project/watcha/domain/movie/controller/MovieController.java
+++ src/main/java/com/project/watcha/domain/movie/controller/MovieController.java
@@ -1,11 +1,25 @@
 package com.project.watcha.domain.movie.controller;
 
+import com.project.watcha.domain.movie.dto.request.UploadMovieDto;
+import com.project.watcha.domain.movie.service.MovieService;
+import com.project.watcha.global.response.ResponseService;
+import com.project.watcha.global.response.result.CommonResultResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class MovieController {
 
+    private final MovieService movieService;
+    private final ResponseService responseService;
 
+    @PostMapping("/upload/movie")
+    public CommonResultResponse uploadMovie(
+            @RequestParam(value = "movieDto") UploadMovieDto uploadMovieDto,
+            @RequestParam(value = "file", required = false) MultipartFile file) {
+        movieService.uploadMovie(uploadMovieDto, file);
+        return responseService.getSuccessResult();
+    }
 }
--- src/main/java/com/project/watcha/domain/movie/enumType/Gender.java
+++
@@ -1,8 +0,0 @@
-package com.project.watcha.domain.movie.enumType;
-
-import lombok.Getter;
-
-@Getter
-public enum Gender {
-    MALE, FEMALE;
-}
