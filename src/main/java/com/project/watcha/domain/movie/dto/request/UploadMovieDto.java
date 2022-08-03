897073306f77a223f57ce9671e1d9b0b5c0b4a89
SungGil-5125 <sunggil0125@naver.com>
2022-08-03 21:07:47 +0900

    ✨ :: Add JsonFormat

--- src/main/java/com/project/watcha/domain/movie/dto/request/UploadMovieDto.java
+++ src/main/java/com/project/watcha/domain/movie/dto/request/UploadMovieDto.java
@@ -1,34 +1,31 @@
 package com.project.watcha.domain.movie.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.watcha.domain.movie.Actor;
 import com.project.watcha.domain.movie.Director;
 import com.project.watcha.domain.movie.Movie;
 import com.project.watcha.domain.movie.enumType.Genre;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Collections;
+import java.util.List;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UploadMovieDto {
 
+    private List<String> director_name;
+    private List<String> actor_name;
     private String title;
     private String content;
     private int year;
+    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
     private Genre genre;
-
-    @JsonCreator
-    public UploadMovieDto(
-            @JsonProperty("title") String title,
-            @JsonProperty("content") String content,
-            @JsonProperty("year") int year,
-            @JsonProperty("genre") Genre genre) {
-        this.title = title;
-        this.content = content;
-        this.year = year;
-        this.genre = genre;
-    }
 
     public Movie toEntity(Director director, Actor actor, String url) {
         return Movie.builder()
