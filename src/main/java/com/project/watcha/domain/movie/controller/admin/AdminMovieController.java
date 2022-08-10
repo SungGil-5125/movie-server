1344f745633674a0b6ad7225a85a1abc9b58373e
SungGil-5125 <sunggil0125@naver.com>
2022-08-10 20:48:39 +0900

    ✨ :: admin이랑 user controller 분리

--- src/main/java/com/project/watcha/domain/movie/controller/admin/AdminMovieController.java
+++ src/main/java/com/project/watcha/domain/movie/controller/admin/AdminMovieController.java
@@ -1,6 +1,7 @@
 package com.project.watcha.domain.movie.controller.admin;
 
 import com.project.watcha.domain.movie.dto.request.UploadMovieDto;
+import com.project.watcha.domain.movie.dto.response.MovieResponseDto;
 import com.project.watcha.domain.movie.service.MovieService;
 import com.project.watcha.global.response.ResponseService;
 import com.project.watcha.global.response.result.CommonResultResponse;
@@ -10,19 +11,33 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/admin")
+@RequestMapping("v1/admin/")
 public class AdminMovieController {
 
     private final MovieService movieService;
     private final ResponseService responseService;
 
-    @PostMapping("/upload/movie")
+    @PostMapping("upload/movie")
     public CommonResultResponse uploadMovie(
             @RequestPart(value = "movieDto") UploadMovieDto uploadMovieDto,
             @RequestParam(value = "image", required = false) MultipartFile imageFile,
             @RequestParam(value = "movie", required = false) MultipartFile movieFile) {
         movieService.uploadMovie(uploadMovieDto, imageFile, movieFile);
         return responseService.getSuccessResult();
+    }
+
+    @PostMapping("")
+
+    @GetMapping("contents/{movie_id}")
+    public CommonResultResponse contentMovie(@PathVariable Long movie_id) {
+        MovieResponseDto data = movieService.contentMovie(movie_id);
+        return responseService.getSingleResult(data);
+    }
+
+    @GetMapping("watch/{movie_id}")
+    public CommonResultResponse watchMovie(@PathVariable Long movie_id) {
+        String data = movieService.watchMovie(movie_id);
+        return responseService.getSingleResult(data);
     }
 
 }
--- src/main/java/com/project/watcha/domain/movie/controller/user/UserMovieController.java
+++ src/main/java/com/project/watcha/domain/movie/controller/user/UserMovieController.java
@@ -1,17 +1,32 @@
 package com.project.watcha.domain.movie.controller.user;
 
+import com.project.watcha.domain.movie.dto.response.MovieResponseDto;
+import com.project.watcha.domain.movie.service.MovieService;
+import com.project.watcha.global.response.ResponseService;
+import com.project.watcha.global.response.result.CommonResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/user")
+@RequestMapping("v1/user/")
 @RequiredArgsConstructor
 public class UserMovieController {
 
-    @GetMapping("test")
-    public String test() {
-        return "성공!";
+    private final MovieService movieService;
+    private final ResponseService responseService;
+
+    @GetMapping("contents/{movie_id}")
+    public CommonResultResponse contentMovie(@PathVariable Long movie_id) {
+        MovieResponseDto data = movieService.contentMovie(movie_id);
+        return responseService.getSingleResult(data);
+    }
+
+    @GetMapping("watch/{movie_id}")
+    public CommonResultResponse watchMovie(@PathVariable Long movie_id) {
+        String data = movieService.watchMovie(movie_id);
+        return responseService.getSingleResult(data);
     }
 }
