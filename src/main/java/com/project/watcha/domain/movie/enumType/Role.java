f9e35730e267b65dd230ac516df0fcabd41093a9
SungGil-5125 <sunggil0125@naver.com>
2022-08-24 12:29:13 +0900

    ✨ :: Add moviePeople Role

--- src/main/java/com/project/watcha/domain/movie/enumType/Role.java
+++ src/main/java/com/project/watcha/domain/movie/enumType/Role.java
@@ -1,8 +1,27 @@
 package com.project.watcha.domain.movie.enumType;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
+
+import java.util.stream.Stream;
 
 @Getter
 public enum Role {
-    DIRECTOR, MAIN_ACTOR, SUPPORTING_ACTOR;
+    DIRECTOR("director"),
+    MAIN_ACTOR("main_actor"),
+    SUPPORTING_ACTOR("supporting_actor");
+
+    private final String code;
+
+    Role(String code) {
+        this.code = code;
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static Role findByCode(String code) {
+        return Stream.of(Role.values())
+                .filter(c -> c.code.equals(code))
+                .findFirst()
+                .orElse(null);
+    }
 }
