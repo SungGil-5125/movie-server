02fb5f2cfda9b6f552796097f3a0bd5e0cb61da7
SungGil-5125 <sunggil0125@naver.com>
2022-08-26 08:04:43 +0900

    ✨ :: Add validation

--- src/main/java/com/project/watcha/domain/sign/dto/request/RefreshTokenDto.java
+++ src/main/java/com/project/watcha/domain/sign/dto/request/RefreshTokenDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @AllArgsConstructor
@@ -15,6 +16,7 @@ import javax.validation.constraints.NotBlank;
 public class RefreshTokenDto {
 
     @Email
+    @Pattern(regexp = "^\\w+@\\w+\\.\\w+(\\.\\w+)?", message = "이메일 형식이 아닙니다")
     @NotBlank
     private String email;
 }
